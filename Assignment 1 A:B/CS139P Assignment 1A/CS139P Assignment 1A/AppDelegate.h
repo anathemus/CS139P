e253440e2f10a6ec35c0649c7e175928112fa07a
BenjaminABurgess <anathemus@gmail.com>
2016-01-04 16:37:30 -0500

    Assignment 1 A&B

---
+++ Assignment 1 A:B/CS139P Assignment 1A/CS139P Assignment 1A/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CS139P Assignment 1A
+//
+//  Created by Benjamin A Burgess on 12/27/15.
+//  Copyright © 2015 Benjamin A Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assignment 1 A:B/CS139P Assignment 1A/CS139P Assignment 1A/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CS139P Assignment 1A
+//
+//  Created by Benjamin A Burgess on 12/27/15.
+//  Copyright © 2015 Benjamin A Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
